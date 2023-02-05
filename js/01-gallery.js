51d7fe589db0def113de697dfd69a6eb7a011d79
Oleg <sirov@ukr.net>
2023-02-05 23:38:05 +0200

    create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const images = galleryItems.map(({ preview, original, description }) => {
 
-console.log(galleryItems);
+    const imageItem = document.createElement("div");
+    imageItem.className = "gallery__item";
+    const imageLink = document.createElement("a");
+    imageItem.className = "gallery__link";
+
+    const image = document.createElement("img");
+    image.src = preview;
+    image.alt = description;
+    image.className = "gallery__image";
+
+    imageLink.append(image);
+    imageItem.append(imageLink);
+
+    return imageItem;
+ }
+);
+gallery.append(...images);
+
+
+
