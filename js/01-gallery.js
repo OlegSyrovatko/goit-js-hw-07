38ee0b23e13fee33c5588a880175642e4bffd56f
Oleg <sirov@ukr.net>
2023-02-07 00:41:53 +0200

    Image gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,38 +3,21 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector(".gallery");
 const images = galleryItems.map(({ preview, original, description }) => {
 
-    // const imageItem = document.createElement("div");
-    // imageItem.className = "gallery__item";
-    // const imageLink = document.createElement("a");
-    // imageLink.className = "gallery__link";
-    // imageLink.href = original;
-
-    // const image = document.createElement("img");
-    // image.className = "gallery__image";
-    // image.src = preview;
-    // image.data_source = original;
-    // image.alt = description;
-    // imageLink.append(image);
-    // imageItem.append(imageLink);
         const imageItem = `<div class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
-          src="${original}"
+          src="${preview}"
           data-source="${original}" 
           alt="${description}"
         />
       </a>
     </div>`;
     return imageItem;
-}
-    
-).join("");
+}).join("");
 
-// gallery.append(...images);
+
 gallery.innerHTML = images;
-
-
 gallery.addEventListener("click", selectImg);
 
 function selectImg(event) { 
@@ -42,10 +25,24 @@ function selectImg(event) {
   if (event.target.nodeName !== "IMG") {
       return;
   }
-    event.target.src = event.target.data-source; 
-    const selectedImg = event.target.src;
-    basicLightbox.create(`
-        <img width="1280" height="871" src="${selectedImg}">
-    `).show()
-} 
+
+    event.preventDefault();
+    event.target.src = event.target.dataset.source; 
+    const selectedImg = event.target.src; 
+    const instance = basicLightbox.create(`<img width="1280" height="871" src="${selectedImg}">`);
+    instance.show()
+
+     document.addEventListener("keydown", event => {
+        const visible = basicLightbox.visible();
+        if (event.key === "Escape" && visible === true) {
+            instance.close();
+         }
+
+     }, {once: true});
+    
+}
+
+
+
+
 
