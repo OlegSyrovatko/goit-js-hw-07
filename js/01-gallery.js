2958c1fa04a19f1c893289e9ce451bc57357204f
Oleg <sirov@ukr.net>
2023-02-06 21:14:05 +0200

    gallery in work...

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,25 +3,36 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector(".gallery");
 const images = galleryItems.map(({ preview, original, description }) => {
 
-    const imageItem = document.createElement("div");
-    imageItem.className = "gallery__item";
-    const imageLink = document.createElement("a");
-    imageItem.className = "gallery__link";
+    // const imageItem = document.createElement("div");
+    // imageItem.className = "gallery__item";
+    // const imageLink = document.createElement("a");
+    // imageLink.className = "gallery__link";
+    // imageLink.href = original;
 
-    const image = document.createElement("img");
-    image.src = preview;
-    image.src_original = original;
-    image.alt = description;
-    image.className = "gallery__image";
-
-    imageLink.append(image);
-    imageItem.append(imageLink);
-
+    // const image = document.createElement("img");
+    // image.className = "gallery__image";
+    // image.src = preview;
+    // image.data_source = original;
+    // image.alt = description;
+    // imageLink.append(image);
+    // imageItem.append(imageLink);
+        const imageItem = `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${original}"
+          data-source="${original}" 
+          alt="${description}"
+        />
+      </a>
+    </div>`;
     return imageItem;
- }
-);
-gallery.append(...images);
+}
+    
+).join("");
 
+// gallery.append(...images);
+gallery.innerHTML = images;
 
 
 gallery.addEventListener("click", selectImg);
@@ -31,7 +42,10 @@ function selectImg(event) {
   if (event.target.nodeName !== "IMG") {
       return;
   }
-  const selectedImg = event.target.src_original; 
-    console.log(selectedImg); 
-
+    event.target.src = event.target.data-source; 
+    const selectedImg = event.target.src;
+    basicLightbox.create(`
+        <img width="1280" height="871" src="${selectedImg}">
+    `).show()
 } 
+
