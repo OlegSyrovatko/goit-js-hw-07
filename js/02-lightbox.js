3acd79447ed768ad3dfc86c687fe0a4f0491db0d
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-02-07 17:25:32 +0200

    gallery2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const images = galleryItems.map(({ preview, original, description }) => {
 
-console.log(galleryItems);
+    const imageItem = `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" title="${description}" />
+    </a>`;
+    return imageItem;
+}).join("");
+
+
+gallery.innerHTML = images;
+
+// let galler = $('.gallery a').simpleLightbox();
+// galler.on('show.simplelightbox', function () {
+//     // do something…
+//     console.log(1);
+// });
+
+
+// let galler = new SimpleLightbox('.gallery a');
+// galler.on('show.simplelightbox', function () {
+//     // do something…
+// });
+
+
+var lightbox = new SimpleLightbox('.gallery a', { captions: true });
